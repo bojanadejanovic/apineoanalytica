f503d5a95990314d5616a2cd24f60dc26c1dcdb9
bojana.dejanovic <bojana.dejanovic@authoritypartners.com>
2020-07-07 16:07:27 +0200

    added Email column to SurveyPariticipant table

--- Database/Tables/SurveyParticipant.sql
+++ Database/Tables/SurveyParticipant.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[SurveyParticipant](
 	[SurveyParticipantId] [int] IDENTITY(1,1) NOT NULL,
 	[LastActiveDate] [datetime] NULL,
+	[Email][nvarchar](100) NULL
 PRIMARY KEY CLUSTERED 
 (
 	[SurveyParticipantId] ASC
