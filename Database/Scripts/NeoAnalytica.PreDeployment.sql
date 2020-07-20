ef457c8a17f827ba582aba315bea62bfc63120a1
bojana.dejanovic <bojana.dejanovic@authoritypartners.com>
2020-07-20 14:07:21 +0200

    added pre/post deployment scripts to db proj

---
+++ Database/Scripts/NeoAnalytica.PreDeployment.sql
@@ -0,0 +1,25 @@
+﻿/*
+ Pre-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be executed before the build script.	
+ Use SQLCMD syntax to include a file in the pre-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the pre-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+
+/* Clean up all the tables */
+
+DELETE FROM [Answer]
+DELETE FROM [Question]
+DELETE FROM [QuestionOption]
+DELETE FROM [QuestionType]
+DELETE FROM [ApplicationRole]
+DELETE FROM [SurveyParticipant]
+DELETE FROM [SurveyCategory]
+DELETE FROM [Survey]
+DELETE FROM [ApplicationUserRole]
+DELETE FROM [ApplicationUser]
