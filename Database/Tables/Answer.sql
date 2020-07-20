df370704c5df340ae7203b2de32563bdb81732d7
bojana.dejanovic <bojana.dejanovic@authoritypartners.com>
2020-07-20 11:35:11 +0200

    added FK between tables

--- Database/Tables/Answer.sql
+++ Database/Tables/Answer.sql
@@ -7,6 +7,27 @@
  CONSTRAINT [PK_Answer_AnswerID] PRIMARY KEY CLUSTERED 
 (
 	[AnswerID] ASC
-)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Answer]  WITH CHECK ADD  CONSTRAINT [FK_Answer_Question_QuestionID] FOREIGN KEY([QuestionID])
+REFERENCES [dbo].[Question] ([QuestionID])
+GO
+
+ALTER TABLE [dbo].[Answer] CHECK CONSTRAINT [FK_Answer_Question_QuestionID]
+GO
+
+ALTER TABLE [dbo].[Answer]  WITH CHECK ADD  CONSTRAINT [FK_Answer_QuestionOption_QuestionOptionID] FOREIGN KEY([SelectedOptionID])
+REFERENCES [dbo].[QuestionOption] ([QuestionOptionID])
+GO
+
+ALTER TABLE [dbo].[Answer] CHECK CONSTRAINT [FK_Answer_QuestionOption_QuestionOptionID]
+GO
+
+ALTER TABLE [dbo].[Answer]  WITH CHECK ADD  CONSTRAINT [FK_Answer_SurveyParticipant_SurveyParticipantID] FOREIGN KEY([SurveyParticipantID])
+REFERENCES [dbo].[SurveyParticipant] ([SurveyParticipantID])
+GO
+
+ALTER TABLE [dbo].[Answer] CHECK CONSTRAINT [FK_Answer_SurveyParticipant_SurveyParticipantID]
 GO
